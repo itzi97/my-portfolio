394913c3aaef3f9aae9536d4eae39c32669c029b
Itziar <56316427+itzi97@users.noreply.github.com>
2026-06-06 16:23:17 +0200

    privacy: shorten name to Itziar Morales, remove phone number

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Itziar Morales Rodríguez';
+export const SITE_TITLE = 'Itziar Morales';
 export const SITE_DESCRIPTION = 'Software Engineering student (minor in Data Science & AI) based in Madrid. Building data pipelines, ML systems, and backend APIs. Open to remote/hybrid internships.';
-export const SITE_AUTHOR = 'Itziar Morales Rodríguez';
+export const SITE_AUTHOR = 'Itziar Morales';
 export const GITHUB_URL = 'https://github.com/itzi97';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/itzi97';
 export const KAGGLE_URL = 'https://www.kaggle.com/itzi97';
 export const EMAIL = 'itzimr97@pm.me';
-export const PHONE = '+34 669 076 611';
 export const CV_URL = '/itziar-morales-cv.pdf';
